00c690a85cf85d02e3bffb8b0303f4257b8ae0af
Jomote <jomote@alumnatflorida.es>
2026-02-17 17:18:07 +0100

    Jasjas
    Nacho guapo

--- backend/listochef/src/main/java/listochef/Test.java
+++ backend/listochef/src/main/java/listochef/Test.java
@@ -5,11 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("Hola");
-<<<<<<< HEAD
+
 		System.out.println("Soy Jose");
-=======
+
 		System.out.println("Soy Bea");
->>>>>>> e8a767e8352c31051fdfd62872f3fe94141c8f5a
+
 
 	}
 
