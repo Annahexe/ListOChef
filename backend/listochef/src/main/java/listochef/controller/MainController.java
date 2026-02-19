4ad92f350311c0aeab7ca7fe54b550596f54c7ac
Iggox <nachoariel2000@gmail.com>
2026-02-19 19:57:52 +0100

    Post endpoint
    post endpoint and small user authentification test

--- backend/listochef/src/main/java/listochef/controller/MainController.java
+++ backend/listochef/src/main/java/listochef/controller/MainController.java
@@ -9,6 +9,8 @@ import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +22,46 @@ import static com.mongodb.client.model.Filters.*;
 
 @RestController
 public class MainController {
-		
+
+	MongoClient mongoClient = new MongoClient("localhost", 27017);
+	MongoDatabase database = mongoClient.getDatabase("ListOChef");
+	MongoCollection<Document> recipesCollection = database.getCollection("recipes");
+	MongoCollection<Document> usersCollection = database.getCollection("users");
+	MongoCursor<Document> cursor;
+
 	@GetMapping("/ListOChef/recipeList")
-	ResponseEntity<Object> recipeList(){
-		MongoClient mongoClient = new MongoClient("localhost", 27017);
-		MongoDatabase database = mongoClient.getDatabase("ListOChef");
-		MongoCollection<Document> coleccion = database.getCollection("recipes");
-		MongoCursor<Document> cursor;
-		
-		//Into para meter toda la info
-		List<Document> list = coleccion.find().into(new ArrayList<>());
-		
+	ResponseEntity<Object> recipeList() {
+
+		// Into para meter toda la info
+		List<Document> list = recipesCollection.find().into(new ArrayList<>());
+
 		return ResponseEntity.status(HttpStatus.OK).body(list);
-				}
+	}
+
+	@PostMapping("/ListOChef/recipeCreate")
+	ResponseEntity<Object> recipeCreate(@RequestBody String recipeBody, @RequestParam String userNickname) {
+
+		Bson query = eq("nickname", userNickname);
+
+		cursor = usersCollection.find(query).iterator();
+
+		try {
+
+			if (cursor.hasNext()) {
+
+				JSONObject jsonUser = new JSONObject(recipeBody);
+				Document doc = Document.parse(jsonUser.toString());
+				recipesCollection.insertOne(doc);
+
+				return ResponseEntity.status(HttpStatus.OK).build();
+
+			} else {
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+			}
+		} catch (Exception e) {
+			
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+
+	}
 }
