f8d293c4acdf6426fb401a5bd3c39b898a06bfd2
Annahexe <119667850+Annahexe@users.noreply.github.com>
2026-02-20 16:31:44 +0100

    update image card height

--- frontend/listochef/src/components/RecipeCard.js
+++ frontend/listochef/src/components/RecipeCard.js
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
     borderTopEndRadius: 15,
     borderTopStartRadius: 15,
     width: "100%",
-    height: 180,
+    height: 150,
     resizeMode: "cover",
   },
   infoContainer: {
