69e51290f6dfd97b9226abb18ba23799f44ebca6
Jomote <jomote@alumnatflorida.es>
2026-02-20 06:38:59 +0100

    register and login
    Trys to create a user login

--- backend/listochef/src/main/java/listochef/controller/MainController.java
+++ backend/listochef/src/main/java/listochef/controller/MainController.java
@@ -20,7 +20,18 @@ import static com.mongodb.client.model.Filters.*;
 
 @RestController
 public class MainController {
+<<<<<<< Updated upstream
 		
+=======
+
+	MongoClient mongoClient = new MongoClient("localhost", 27017);
+	MongoDatabase database = mongoClient.getDatabase("ListOChef");
+	MongoCollection<Document> recipesCollection = database.getCollection("recipes");
+	MongoCollection<Document> usersCollection = database.getCollection("users");
+	MongoCursor<Document> cursor;
+	static String user;
+
+>>>>>>> Stashed changes
 	@GetMapping("/ListOChef/recipeList")
 	ResponseEntity<Object> recipeList(){
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
@@ -32,5 +43,73 @@ public class MainController {
 		List<Document> list = coleccion.find().into(new ArrayList<>());
 		
 		return ResponseEntity.status(HttpStatus.OK).body(list);
+<<<<<<< Updated upstream
 				}
+=======
+	}
+
+	@PostMapping("/ListOChef/recipeCreate")
+	ResponseEntity<Object> recipeCreate(@RequestBody String recipeBody, @RequestParam String userNickname) {
+
+		Bson query = eq("nickname", userNickname);
+
+		cursor = usersCollection.find(query).iterator();
+
+		try {
+
+			if (cursor.hasNext()) {
+
+				JSONObject jsonUser = new JSONObject(recipeBody);
+				Document doc = Document.parse(jsonUser.toString());
+				recipesCollection.insertOne(doc);
+
+				return ResponseEntity.status(HttpStatus.OK).build();
+
+			} else {
+				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+			}
+		} catch (Exception e) {
+			
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+
+	}
+	
+	@PostMapping("/ListOChef/createUser")
+	ResponseEntity<Object> createUser(@RequestBody String data){
+		JSONObject jsondata = new JSONObject(data);
+		String nickName = jsondata.getString("nickname");
+		String email = jsondata.getString("email");
+		String password = jsondata.getString("password");
+		Bson query = eq("nickname", nickName);
+		cursor = usersCollection.find(query).iterator();
+		if(cursor.hasNext()) {
+			Document doc = new Document();
+			doc.append("nickname", nickName);
+			doc.append("email",email);
+			doc.append("password", password);
+			doc.append("avatar", "");
+			usersCollection.insertOne(doc);
+			return ResponseEntity.status(HttpStatus.OK).build();
+		}else {
+			System.out.println("Ya existe este usuario en la base de datos");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+	}
+	@PostMapping("/ListOChef/login")
+	ResponseEntity<Object> login(@RequestBody String data){
+		JSONObject jsondata = new JSONObject(data);
+		String nickName = jsondata.getString("nickname");
+		String password = jsondata.getString("password");
+		Bson query = and(eq("nickname", nickName), eq("password",password));
+		cursor = usersCollection.find(query).iterator();
+		if(cursor.hasNext()) {
+			user = nickName;
+			return ResponseEntity.status(HttpStatus.OK).build();
+		}else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+		
+	}
+>>>>>>> Stashed changes
 }
