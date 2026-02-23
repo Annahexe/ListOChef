9cb6912e79b05412dbb0c2e94f5f116b4676446c
Annahexe <119667850+Annahexe@users.noreply.github.com>
2026-02-23 22:59:01 +0100

    added carousel of food tags for filtering of recipes

---
+++ frontend/listochef/src/components/TagsCarousel.js
@@ -0,0 +1,47 @@
+import { View, Pressable, StyleSheet, ScrollView, Text } from "react-native";
+
+const FilterTag = (props) => {
+  const tag = props.tag;
+  const isSelected = props.isSelected;
+
+  return (
+    <Pressable onPress={() => props.onPressTag(tag)} style={[styles.tag, isSelected && styles.tagSelected]}>
+      <Text style={styles.tagText}>{tag}</Text>
+    </Pressable>
+  );
+};
+
+export const TagsCarousel = (props) => {
+  const tagsList = props.tagsList;
+  const selectedTags = props.selectedTags ?? [];
+  const onToggleTag = props.onToggleTag;
+
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {tagsList.map((tagItem) => (
+          <FilterTag key={tagItem} tag={tagItem} isSelected={selectedTags.includes(tagItem)} onPressTag={onToggleTag} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { width: "95%", marginVertical: "3%" },
+  tag: {
+    paddingHorizontal: 12,
+    paddingVertical: 3,
+    borderRadius: 10,
+    marginRight: 10,
+    backgroundColor: "rgba(75, 100, 63, 0.5)",
+  },
+  tagSelected: {
+    backgroundColor: "#4B643F",
+  },
+  tagText: {
+    fontFamily: "MontserratSemiBold",
+    color: "white",
+    fontSize: 16,
+  },
+});
--- frontend/listochef/src/screens/RecipeList/SearchRecipes.js
+++ frontend/listochef/src/screens/RecipeList/SearchRecipes.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, ImageBackground, Pressable, ScrollView } from "react-native";
 import { useEffect, useState } from "react";
 import { Seeker } from "../../components/Seeker";
+import { TagsCarousel } from "../../components/TagsCarousel";
 import RecipeCard from "../../components/RecipeCard";
 import Feather from "@expo/vector-icons/Feather";
 
@@ -9,41 +10,74 @@ const SearchRecipes = (props) => {
   const [recipeList, setRecipeList] = useState([]);
   const [results, setResults] = useState([]);
 
+  const TAGS = ["All", "Pasta", "Fish", "Pork", "Beef", "Chicken", "Meat"];
+
+  const [selectedTags, setSelectedTags] = useState(["All"]);
+
+  const toggleTag = (selectedTag) => {
+    setSelectedTags((previousSelectedTags) => {
+      if (selectedTag == "All") {
+        return ["All"];
+      }
+
+      const tagsWithoutAll = previousSelectedTags.filter((element) => element !== "All");
+
+      if (tagsWithoutAll.includes(selectedTag)) {
+        const selectedTagsList = tagsWithoutAll.filter((element) => element !== selectedTag);
+        
+        return selectedTagsList.length === 0 ? ["All"] : selectedTagsList;
+      }
+
+      return [...tagsWithoutAll, selectedTag];
+    });
+  };
+
   //demo data
   useEffect(() => {
     setRecipeList([
       {
         id: "1",
         recipeName: "Spaghetti Bolognese",
-        type: "pasta",
+        type: "lunch",
+        tags: ["pasta", "meat", "easy"],
         time: 20,
         difficulty: "easy",
-        photo:
-          "https://supervalu.ie/image/var/files/real-food/recipes/Uploaded-2020/spaghetti-bolognese-recipe.jpg",
+        photo: "https://supervalu.ie/image/var/files/real-food/recipes/Uploaded-2020/spaghetti-bolognese-recipe.jpg",
         creationDate: "17/02/2026",
         isSaved: true,
       },
       {
         id: "2",
         recipeName: "Paella",
-        type: "arroz, conejo",
+        type: "rice",
+        tags: ["pasta", "meat", "easy"],
         time: 60,
         difficulty: "hard",
-        photo:
-          "https://e00-xlk-cooking-elmundo.uecdn.es/files/article_main_microformat_4_3/uploads/2023/02/28/63fe82e0ba614.jpeg",
+        photo: "https://e00-xlk-cooking-elmundo.uecdn.es/files/article_main_microformat_4_3/uploads/2023/02/28/63fe82e0ba614.jpeg",
         creationDate: "18/02/2026",
         isSaved: false,
       },
       {
         id: "3",
         recipeName: "Bolognese Sauce",
-        type: "sauce, meat, vegetable",
+        type: "sauce",
+        tags: ["pasta", "meat", "easy"],
         time: 30,
         difficulty: "medium",
-        photo:
-          "https://www.healthyfood.com/wp-content/uploads/2016/11/Bolognese-sauce-iStock-485714898.jpg",
+        photo: "https://www.healthyfood.com/wp-content/uploads/2016/11/Bolognese-sauce-iStock-485714898.jpg",
         creationDate: "15/02/2026",
         isSaved: true,
+      },
+      {
+        id: "4",
+        recipeName: "Gnocchi Bolognese",
+        type: "Lunch",
+        tags: ["pasta", "meat", "normal"],
+        time: 30,
+        difficulty: "medium",
+        photo: "https://www.eatclub.de/wp-content/uploads/2024/01/gnocchi-bolognese.jpg",
+        creationDate: "15/02/2026",
+        isSaved: false,
       },
     ]);
   }, []);
@@ -60,13 +94,14 @@ const SearchRecipes = (props) => {
             <Feather name="chevron-left" size={60} color="rgba(75, 100, 63, 0.7)" onPress={() => props.navigation.goBack()} />
             <Seeker placeholderText="Search recipe..." onPress={() => console.log("searching")}></Seeker>
           </View>
-            <ScrollView style={{ width: "100%", marginBottom: 15 }} contentContainerStyle={{ paddingBottom: 20 }}>
-              {recipeList.map((recipe, index) => (
-                <Pressable key={index} onPress={onViewRecipe}>
-                  <RecipeCard name={recipe.recipeName} isSaved={recipe.isSaved} image={recipe.photo}></RecipeCard>
-                </Pressable>
-              ))}
-            </ScrollView>
+          <TagsCarousel tagsList={TAGS} selectedTags={selectedTags} onToggleTag={toggleTag} />
+          <ScrollView style={{ width: "100%", marginBottom: "12%" }} contentContainerStyle={{ paddingBottom: 20 }}>
+            {recipeList.map((recipe, index) => (
+              <Pressable key={index} onPress={onViewRecipe}>
+                <RecipeCard name={recipe.recipeName} isSaved={recipe.isSaved} image={recipe.photo}></RecipeCard>
+              </Pressable>
+            ))}
+          </ScrollView>
         </View>
       </View>
     </ImageBackground>
