80fa74d3634c62f0ff2e40131bd3f6f571469c6c
Jomote <jomote@alumnatflorida.es>
2026-02-27 09:32:11 +0100

    pom corrections
    Correcciones para correcto funcionamiento online

--- backend/listochef/src/main/java/listochef/controller/MainController.java
+++ backend/listochef/src/main/java/listochef/controller/MainController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mongodb.MongoClient;
+//import com.mongodb.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
