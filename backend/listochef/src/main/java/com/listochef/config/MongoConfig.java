713374f8903a84827f595a24ee3dc9c72aa2de79
Iggox <nachoariel2000@gmail.com>
2026-03-03 01:50:30 +0100

    Config working with app.prop
    Config working as intended using application.properties

--- backend/listochef/src/main/java/com/listochef/config/MongoConfig.java
+++ backend/listochef/src/main/java/com/listochef/config/MongoConfig.java
@@ -3,19 +3,34 @@ package com.listochef.config;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MongoConfig {
+	
+	@Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
 
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @PostConstruct
+    public void test() {
+        System.out.println("Mongo URI: " + mongoUri);
+    }
+    
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create(mongoUri);
     }
 
     @Bean
     public MongoDatabase mongoDatabase(MongoClient mongoClient) {
-        return mongoClient.getDatabase("ListOChef");
+        return mongoClient.getDatabase(databaseName);
     }
 }
